9b7c298d6772f59b38fcf6bef4280e95f4b45dd7
Nathan Luong <nhanto20021@gmail.com>
2021-06-26 18:45:41 -0400

    Finish Backend

--- app.py
+++ app.py
@@ -1,21 +1,64 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, url_for, session
 from flaskext.mysql import MySQL
-from database_access import add_level, add_user
-app = Flask(__name__)
+from database_access import add_level, add_user, get_user_num, get_user_data, count_duplicate_user
+from forms import LoginForm, RegisterForm
 
+app = Flask(__name__)
 mysql = MySQL()
 
 app.config['MYSQL_DATABASE_USER'] = 'bdbc2dbdbda579'
 app.config['MYSQL_DATABASE_PASSWORD'] = '6875d2e1'
 app.config['MYSQL_DATABASE_DB'] = 'heroku_c9fdd1a5f9509b9'
 app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-04.cleardb.com'
+app.config['SECRET_KEY'] = 'my-name-is-Jeff'
 
 mysql.init_app(app)
 db = mysql.connect()
 
-
-
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', username = session["username"])
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+    if "username" in session:
+        return redirect(url_for('home'))
+    else:
+        login_form = LoginForm()
+        if login_form.validate_on_submit():
+
+            pss = get_user_data(db, login_form.username.data)[2]
+            psswd_input = login_form.password.data
+            
+            if psswd_input == pss:
+                session["username"] = login_form.username.data
+                return redirect(url_for('home'))
+
+            return redirect(url_for('register'))
+        return render_template( 'login.html', form = login_form )
+
+
+@app.route('/register', methods=['POST','GET'])
+def register():
+    register_form = RegisterForm()
+    same_pass = True
+    duplicate = False
+    if register_form.validate_on_submit():
+        #flash(f'Login requested for user {register_form.username.data}')
+        if register_form.password.data == register_form.confirm_password.data:
+            duplicate_users_num = count_duplicate_user(db, register_form.username.data)
+            if duplicate_users_num == 0:
+                print("Im in here")
+                num = get_user_num(db)
+                add_user(db, num+1, register_form.username.data, register_form.password.data)
+                return redirect(url_for('home'))
+            else:
+                duplicate = True
+        else:
+            same_pass = False
+    return render_template('register.html', form = register_form, same_pass = same_pass, duplicate = duplicate)
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
--- database_access.py
+++ database_access.py
@@ -1,9 +1,7 @@
 def add_level(db, level_id, name, data, des = 'null' , comment = 'null'):
     sqlcursor = db.cursor()
-
     db.ping()
     sqlcursor.execute(f" INSERT INTO levels VALUES({level_id}, \"{name}\", \"{data}\", \"{des}\", 0, 0, \"{comment}\") ")
-
     db.commit()
     sqlcursor.close()
     db.close()
@@ -12,24 +10,53 @@ def add_level(db, level_id, name, data, des = 'null' , comment = 'null'):
 
 def add_user(db, user_id, username, password, liked_levels = 'null', created = 'null', beaten='null'):
     sqlcursor = db.cursor()
-
     db.ping()
     sqlcursor.execute(f" INSERT INTO user VALUES({user_id}, \"{username}\", \"{password}\", \"{liked_levels}\", \"{created}\", \"{beaten}\") ")
-
     db.commit()
     sqlcursor.close()
     db.close()
     return 
 
+def get_user_num(db):
+    sqlcursor = db.cursor()
+    db.ping()
+    sqlcursor.execute(" SELECT COUNT(`id`) FROM user ")
+    num = sqlcursor.fetchone()[0]
+    db.commit()
+    sqlcursor.close()
+    db.close()
+    return num
+
+def get_user_data(db, user_name):
+    sqlcursor = db.cursor()
+    db.ping()
+
+    sqlcursor.execute(f" SELECT * FROM user WHERE `username`= \"{user_name}\" ")
+
+    data = sqlcursor.fetchone()
+
+    db.commit()
+    sqlcursor.close()
+    db.close()
+    
+    return data
+
+def count_duplicate_user(db, user_name):
+    sqlcursor = db.cursor()
+    db.ping()  
+    sqlcursor.execute(f" SELECT COUNT(id) FROM user WHERE `username`= \"{user_name}\" ")
+    num = sqlcursor.fetchone()[0]
+    db.commit()
+    sqlcursor.close()
+    db.close()
+    return num
+
 def read_user_table(db):
     sqlcursor = db.cursor()
-
     db.ping()
     sqlcursor.execute(" SELECT * FROM user ")
-
     for i in sqlcursor:
         print(i)
-
     db.commit()
     sqlcursor.close()
     db.close()
